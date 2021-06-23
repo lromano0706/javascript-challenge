4fe0f8fba6dca5afb49b825ba984819553a8d2ce
lromano0706 <78659955+lromano0706@users.noreply.github.com>
2021-06-22 22:04:33 -0700

    updated html and js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(date => date.datetime === inputValue);
+
+  console.log(filteredData);
+
+
+};
